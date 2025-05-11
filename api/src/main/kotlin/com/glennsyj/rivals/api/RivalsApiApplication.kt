22807c4cb4494280c069ebf2e243f4a0853861ca
glenn-syj <glenn.syj@gmail.com>
2025-05-11 17:14:10 +0900

    Initialize kotlin based api server

---
+++ api/src/main/kotlin/com/glennsyj/rivals/api/RivalsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.glennsyj.rivals.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RivalsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RivalsApiApplication>(*args)
+}
