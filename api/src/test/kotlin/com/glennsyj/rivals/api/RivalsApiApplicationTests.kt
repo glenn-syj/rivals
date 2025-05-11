2c6400744c3937b7c71c1706ab9806118991b053
glenn-syj <glenn.syj@gmail.com>
2025-05-11 17:14:10 +0900

    Initialize kotlin based api server

---
+++ api/src/test/kotlin/com/glennsyj/rivals/api/RivalsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glennsyj.rivals.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RivalsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
