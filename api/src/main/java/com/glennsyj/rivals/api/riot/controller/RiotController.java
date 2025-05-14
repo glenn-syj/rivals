9254af0ea992db50e5f1d61ad9ac5bf7810cb011
glenn-syj <glenn.syj@gmail.com>
2025-05-14 23:50:05 +0900

    Add RiotController for managing Riot accounts
    - Implemented RESTful endpoints for account retrieval and renewal
    - Integrated RiotAccountManager for account operations

---
+++ api/src/main/java/com/glennsyj/rivals/api/riot/controller/RiotController.java
@@ -0,0 +1,47 @@
+package com.glennsyj.rivals.api.riot.controller;
+
+import com.glennsyj.rivals.api.riot.entity.RiotAccount;
+import com.glennsyj.rivals.api.riot.model.RiotAccountResponse;
+import com.glennsyj.rivals.api.riot.service.RiotAccountManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/riot")
+public class RiotController {
+
+    private final RiotAccountManager riotAccountManager;
+
+    public RiotController(RiotAccountManager riotAccountManager) {
+        this.riotAccountManager = riotAccountManager;
+    }
+
+    @GetMapping("/accounts/{gameName}/{tagLine}")
+    public ResponseEntity<RiotAccountResponse> findAccount(@PathVariable("gameName") String gameName
+            , @PathVariable("tagLine") String tagLine) {
+
+        RiotAccount account = riotAccountManager.findOrRegisterAccount(gameName, tagLine);
+        RiotAccountResponse response = new RiotAccountResponse(account.getPuuid(),
+                account.getGameName(),
+                account.getTagLine());
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/accounts/renew/{gameName}/{tagLine}")
+    public ResponseEntity<RiotAccountResponse> renewAccount(@PathVariable("gameName") String gameName
+            , @PathVariable("tagLine") String tagLine) {
+
+        RiotAccount account = riotAccountManager.renewAccount(gameName, tagLine);
+        RiotAccountResponse response = new RiotAccountResponse(account.getPuuid(),
+                account.getGameName(),
+                account.getTagLine());
+
+        return ResponseEntity.ok(response);
+    }
+
+}
