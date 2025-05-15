13451df357fc35fe9ebf98e2f186ecb36a12188c
glenn-syj <glenn.syj@gmail.com>
2025-05-16 02:09:48 +0900

    Add TftLeagueEntryRepository
    - Created TftLeagueEntryRepository interface to facilitate database operations for TftLeagueEntry entities.
    - Included methods to retrieve league information by player puuid and account ID, enhancing data access for TFT league entries.

---
+++ api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftLeagueEntryRepository.java
@@ -0,0 +1,23 @@
+package com.glennsyj.rivals.api.tft.repository;
+
+import com.glennsyj.rivals.api.tft.entity.TftLeagueEntry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TftLeagueEntryRepository extends JpaRepository<TftLeagueEntry, Long> {
+
+    /**
+     * RiotAccount의 puuid로 TFT 리그 정보 조회
+     * 일반 TFT 랭크 정보만 조회 (초고속 제외)
+     */
+    Optional<TftLeagueEntry> findByPuuid(String puuid);
+
+    /**
+     * RiotAccount의 id로 TFT 리그 정보 조회
+     * 일반 TFT 랭크 정보만 조회 (초고속 제외)
+     */
+    Optional<TftLeagueEntry> findByAccount_Id(Long account_id);
+}
