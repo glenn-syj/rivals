af362c4800848f202d6815a6352ccaaab45166cf
glenn-syj <glenn.syj@gmail.com>
2025-05-16 02:08:24 +0900

    Add MariaDB driver class name to TestContainerConfig

--- api/src/test/java/com/glennsyj/rivals/api/config/TestContainerConfig.java
+++ api/src/test/java/com/glennsyj/rivals/api/config/TestContainerConfig.java
@@ -1,14 +1,17 @@
 package com.glennsyj.rivals.api.config;
 
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 import javax.sql.DataSource;
 
 @TestConfiguration
 public class TestContainerConfig {
+
     private static final MariaDBContainer<?> mariaDB;
 
     static {
@@ -30,6 +33,7 @@ public class TestContainerConfig {
                 .url(mariaDB.getJdbcUrl())
                 .username(mariaDB.getUsername())
                 .password(mariaDB.getPassword())
+                .driverClassName(mariaDB.getDriverClassName())
                 .build();
     }
 }
