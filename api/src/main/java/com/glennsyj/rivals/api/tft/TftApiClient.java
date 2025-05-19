93804db4477fe54632eaf2dc6a92dbe24083852e
glenn-syj <glenn.syj@gmail.com>
2025-05-19 23:45:30 +0900

    Refactor getLeagueEntries

--- api/src/main/java/com/glennsyj/rivals/api/tft/TftApiClient.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/TftApiClient.java
@@ -3,6 +3,7 @@ package com.glennsyj.rivals.api.tft;
 import com.glennsyj.rivals.api.common.client.BaseRiotClient;
 import com.glennsyj.rivals.api.tft.model.TftLeagueEntryResponse;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,10 +19,10 @@ public class TftApiClient extends BaseRiotClient {
         return handleApiCall(
                 riotKorWebClient.get()
                         .uri("/tft/league/v1/by-puuid/{puuid}", puuid)
+                        .accept(MediaType.APPLICATION_JSON)
                         .retrieve()
-                        .bodyToMono(new ParameterizedTypeReference<List<TftLeagueEntryResponse>>() {})
-                        .map(entries -> entries.stream()
-                                .toList()),
+                        .bodyToMono(new ParameterizedTypeReference<List<TftLeagueEntryResponse>>() {
+                        }),
                 "소환사의 TFT 리그 정보를 찾을 수 없습니다: " + puuid
         );
     }
