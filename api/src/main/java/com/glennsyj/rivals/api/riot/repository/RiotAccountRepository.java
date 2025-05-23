aec07597c2a57a548859abef0acbe273185b3766
glenn-syj <glenn.syj@gmail.com>
2025-05-24 03:11:10 +0900

    Add method to retrieve multiple RiotAccounts by their IDs
    - Introduced findAllByIdIn method in RiotAccountRepository to fetch a list of RiotAccount entities based on a list of provided IDs.

--- api/src/main/java/com/glennsyj/rivals/api/riot/repository/RiotAccountRepository.java
+++ api/src/main/java/com/glennsyj/rivals/api/riot/repository/RiotAccountRepository.java
@@ -1,10 +1,12 @@
 package com.glennsyj.rivals.api.riot.repository;
 
 import com.glennsyj.rivals.api.riot.entity.RiotAccount;
+import com.glennsyj.rivals.api.rivalry.entity.Rivalry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +14,5 @@ public interface RiotAccountRepository extends JpaRepository<RiotAccount, Long> 
     // 추후 성능 상 문제 발생할 시: Projection 변경 고려 (조회용)
     Optional<RiotAccount> findByGameNameAndTagLine(String gameName, String tagLine);
     Optional<RiotAccount> findByPuuid(String puuid);
+    List<RiotAccount> findAllByIdIn(List<Long> ids);
 }
