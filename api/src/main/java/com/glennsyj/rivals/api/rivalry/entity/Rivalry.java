a0bb16bdd5bc643362d80a8c2f4fa085a601523c
glenn-syj <glenn.syj@gmail.com>
2025-05-23 15:36:00 +0900

    Add Rivalry and RivalryParticipant entities
    - Introduced the Rivalry entity to represent rivalry records, including a list of participants.
    - Added the RivalryParticipant entity to manage participants in a rivalry, linking to RiotAccount and including a side enumeration.

---
+++ api/src/main/java/com/glennsyj/rivals/api/rivalry/entity/Rivalry.java
@@ -0,0 +1,44 @@
+package com.glennsyj.rivals.api.rivalry.entity;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="rivalries")
+@EntityListeners(AuditingEntityListener.class)
+public class Rivalry {
+
+    @Id
+    @Tsid
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // 소환사의 협곡 랭크로 확장 시 고려
+    // 각 Side 마다 특정 인원이 동시에 포함된 비교가 필요할 수 있으므로 Set 대신 List
+    @OneToMany(mappedBy = "rivalry")
+    private List<RivalryParticipant> participants = new ArrayList<>();
+
+    protected Rivalry() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public List<RivalryParticipant> getParticipants() {
+        return participants;
+    }
+}
---
+++ api/src/main/java/com/glennsyj/rivals/api/rivalry/entity/RivalryParticipant.java
@@ -0,0 +1,63 @@
+package com.glennsyj.rivals.api.rivalry.entity;
+
+import com.glennsyj.rivals.api.riot.entity.RiotAccount;
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "rivalry_participants")
+public class RivalryParticipant {
+
+    @Id
+    @Tsid
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "riot_account_id", nullable = false)
+    private RiotAccount riotAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rivalry_id", nullable = false)
+    private Rivalry rivalry;
+
+    @Enumerated(EnumType.STRING)
+    private RivalSide side;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    protected RivalryParticipant() {}
+
+    public RivalryParticipant(RiotAccount riotAccount, Rivalry rivalry, RivalSide side) {
+        this.riotAccount = riotAccount;
+        this.rivalry = rivalry;
+        this.side = side;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public RiotAccount getRiotAccount() {
+        return riotAccount;
+    }
+
+    public Rivalry getRivalry() {
+        return rivalry;
+    }
+
+    public RivalSide getSide() {
+        return side;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
