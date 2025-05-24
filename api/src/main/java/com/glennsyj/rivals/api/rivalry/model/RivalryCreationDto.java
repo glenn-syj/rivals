c405d8aeb1cce0bdf4354231dd9dc0beb515caf3
glenn-syj <glenn.syj@gmail.com>
2025-05-25 01:48:44 +0900

    Add validation and participant checks to RivalryCreationDto
    - Added validation to ensure the participants list is not empty.
    - Implemented logic to verify that at least one participant is present for both LEFT and RIGHT sides during RivalryCreationDto instantiation.
    - Updated unit tests based on the validation.

--- api/src/main/java/com/glennsyj/rivals/api/rivalry/model/RivalryCreationDto.java
+++ api/src/main/java/com/glennsyj/rivals/api/rivalry/model/RivalryCreationDto.java
@@ -1,8 +1,25 @@
 package com.glennsyj.rivals.api.rivalry.model;
 
+import com.glennsyj.rivals.api.rivalry.entity.RivalSide;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public record RivalryCreationDto(
-    List<RivalryParticipantDto> participants
+        @NotEmpty(message="participants should not be empty")
+        List<RivalryParticipantDto> participants
 ) {
+    public RivalryCreationDto {
+
+        // LEFT SIDE와 RIGHT SIDE에 최소한 하나의 참여자는 있도록 함
+        boolean hasLeft = participants.stream()
+                .anyMatch(p -> p.side() == RivalSide.LEFT);
+        boolean hasRight = participants.stream()
+                .anyMatch(p -> p.side() == RivalSide.RIGHT);
+
+        if (!hasLeft || !hasRight) {
+            throw new IllegalArgumentException(
+                    "At least one participant required for each side (LEFT and RIGHT)");
+        }
+    }
 }
