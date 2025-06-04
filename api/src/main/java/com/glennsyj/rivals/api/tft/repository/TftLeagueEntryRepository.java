a16496a0124975fcae88a4a4125954c82961b119
glenn-syj <glenn.syj@gmail.com>
2025-06-04 12:44:42 +0900

    Add a method to retrieve the latest entries by queue type
    - Added the `findLatestEntriesForEachQueueTypeByAccountId` method to retrieve the latest `TftLeagueEntry` for each queue type based on the `accountId`.

--- api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftLeagueEntryRepository.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftLeagueEntryRepository.java
@@ -2,6 +2,8 @@ package com.glennsyj.rivals.api.tft.repository;
 
 import com.glennsyj.rivals.api.tft.entity.TftLeagueEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -28,4 +30,15 @@ public interface TftLeagueEntryRepository extends JpaRepository<TftLeagueEntry, 
 
     Optional<TftLeagueEntry> findFirstByAccount_IdOrderByUpdatedAtDesc(Long accountId);
 
+    @Query("""
+        SELECT e1 FROM TftLeagueEntry e1
+        WHERE e1.account.id = :accountId
+        AND e1.updatedAt = (
+            SELECT MAX(e2.updatedAt)
+            FROM TftLeagueEntry e2
+            WHERE e2.account.id = e1.account.id
+            AND e2.queueType = e1.queueType
+        )
+    """)
+    List<TftLeagueEntry> findLatestEntriesForEachQueueTypeByAccountId(@Param("accountId") Long accountId);
 }
