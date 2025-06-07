3e3ad8c42dc39cfd744faaeda1b6ae18bb61af56
glenn-syj <glenn.syj@gmail.com>
2025-06-08 00:35:39 +0900

    Add new test method to validate match information API call based on match ID
    - Added a test method in the TftApiClientIntegrationTest class to validate the TFT match ID with an actual API call.
    - Included logic to check that the match ID list is not empty using puuid.
    - Used the first match ID to call match information from the API and confirmed that the response is not null.

--- api/src/test/java/com/glennsyj/rivals/api/tft/TftApiClientIntegrationTest.java
+++ api/src/test/java/com/glennsyj/rivals/api/tft/TftApiClientIntegrationTest.java
@@ -2,6 +2,7 @@ package com.glennsyj.rivals.api.tft;
 
 import com.glennsyj.rivals.api.riot.RiotAccountClient;
 import com.glennsyj.rivals.api.tft.model.entry.TftLeagueEntryResponse;
+import com.glennsyj.rivals.api.tft.model.match.TftMatchResponse;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -65,5 +66,29 @@ public class TftApiClientIntegrationTest {
         assertNotNull(matchIds);
         // 매치 ID 리스트가 비어있지 않음을 확인 -> 그러나 실제 로직에서는 비어있는 경우도 고려
         assertThat(matchIds).isNotEmpty();
+    }
+
+    @Test
+    void TFT_매치_ID_기반_매치_정보_실제API호출_테스트() throws InterruptedException {
+        // given
+        String gameName = "승상싱";
+        String tagLine = "KR1";
+
+        String puuid = riotAccountClient.getAccountInfo(gameName, tagLine).puuid();
+        List<String> matchIds = tftApiClient.getMatchIdsFromPuuid(puuid);
+
+        // 매치 ID가 존재하는지 확인
+        // 존재하지 않으면 다른 유저 정보 이용 시도해야 함
+        assertNotNull(matchIds);
+        assertThat(matchIds).isNotEmpty();
+
+        String matchId = matchIds.get(0); // 첫 번째 매치 ID를 사용
+
+        // when
+        TftMatchResponse matchResponse = tftApiClient.getMatchResponseFromMatchId(matchId);
+
+        // then
+        assertNotNull(matchResponse);
+        log.debug("Match Response: {}", matchResponse);
     }
 }
