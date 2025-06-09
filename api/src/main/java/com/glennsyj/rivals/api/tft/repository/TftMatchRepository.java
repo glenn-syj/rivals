441ca2b891035bcc429898a255681dfd111da3b2
glenn-syj <glenn.syj@gmail.com>
2025-06-10 00:22:13 +0900

    Add a method to TftMatchRepository to retrieve the latest 20 games
    - Implemented the findTop20ByParticipantsPuuidOrderByGameCreationDesc method in the TftMatchRepository interface to return the latest 20 games for a specific participant, sorted by game creation date.

--- api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftMatchRepository.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftMatchRepository.java
@@ -3,5 +3,10 @@ package com.glennsyj.rivals.api.tft.repository;
 import com.glennsyj.rivals.api.tft.entity.match.TftMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TftMatchRepository extends JpaRepository<TftMatch, Long> {
+
+    List<TftMatch> findTop20ByParticipantsPuuidOrderByGameCreationDesc(String puuid);
+
 }
