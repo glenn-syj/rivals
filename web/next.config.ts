c2b4c740a48ab6d268dd39e7f0a487a9bce37202
glenn-syj <glenn.syj@gmail.com>
2025-06-12 23:17:22 +0900

    Add image domain to Next.js configuration
    - Allow loading external images from 'ddragon.leagueoflegends.com'.

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: "standalone",
+  images: {
+    domains: ["ddragon.leagueoflegends.com"],
+  },
 };
 
 export default nextConfig;
