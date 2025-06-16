334c637d17bc712230813b7f9914b03c4b776c1f
glenn-syj <glenn.syj@gmail.com>
2025-06-16 12:19:42 +0900

    Add new AchievementType enum and TftBadgeProgress class
    - Introduced AchievementType enum to define TFT achievement types.
    - Implemented TftBadgeProgress class to establish a relationship with RiotAccount and manage achievement progress.
    - Added BadgeType enum to define the required counts for each achievement type and included a method to update progress.

---
+++ api/src/main/java/com/glennsyj/rivals/api/tft/entity/achievement/AchievementType.java
@@ -0,0 +1,9 @@
+package com.glennsyj.rivals.api.tft.entity.achievement;
+
+public enum AchievementType {
+    MOST_EXPENSIVE_SQUAD,
+    MOST_DAMAGE_DEALT,
+    MOST_ELIMINATIONS,
+    FIRST_PLACE,
+    TOP_FOUR
+}
