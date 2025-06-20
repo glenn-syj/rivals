9b9079082c1a9cadb5bcb3db016fb08fe0deada8
glenn-syj <glenn.syj@gmail.com>
2025-06-20 17:45:29 +0900

    Refactor RiotController and add DTO usage
    - Added logic to convert RiotAccount entity to DTO using RiotAccountDto.
    - Modified account retrieval and renewal methods to return DTOs for improved response format.
    - Changed the HTTP method for the renewal method to PATCH.
    - Added related test cases to validate the account renewal functionality

--- api/src/main/java/com/glennsyj/rivals/api/riot/controller/RiotController.java
+++ api/src/main/java/com/glennsyj/rivals/api/riot/controller/RiotController.java
@@ -1,6 +1,7 @@
 package com.glennsyj.rivals.api.riot.controller;
 
 import com.glennsyj.rivals.api.riot.entity.RiotAccount;
+import com.glennsyj.rivals.api.riot.model.RiotAccountDto;
 import com.glennsyj.rivals.api.riot.model.RiotAccountResponse;
 import com.glennsyj.rivals.api.riot.service.RiotAccountManager;
 import com.glennsyj.rivals.api.tft.service.TftLeagueEntryManager;
@@ -8,65 +9,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/riot")
 public class RiotController {
 
     private final RiotAccountManager riotAccountManager;
-    private final TftLeagueEntryManager tftLeagueEntryManager;
 
     private final Logger log = LoggerFactory.getLogger(RiotController.class);
 
-    public RiotController(RiotAccountManager riotAccountManager, TftLeagueEntryManager tftLeagueEntryManager) {
-
+    public RiotController(RiotAccountManager riotAccountManager) {
         this.riotAccountManager = riotAccountManager;
-        this.tftLeagueEntryManager = tftLeagueEntryManager;
     }
 
     @GetMapping("/accounts/{gameName}/{tagLine}")
-    public ResponseEntity<?> findAccount(@PathVariable("gameName") String gameName
+    public ResponseEntity<RiotAccountDto> findAccount(@PathVariable("gameName") String gameName
             , @PathVariable("tagLine") String tagLine) {
-
         try {
             RiotAccount account = riotAccountManager.findOrRegisterAccount(gameName, tagLine);
-            RiotAccountResponse response = new RiotAccountResponse(account.getPuuid(),
-                account.getGameName(),
-                account.getTagLine(),
-                String.valueOf(account.getId()));
-
-            return ResponseEntity.ok(response);
-
+            return ResponseEntity.ok(RiotAccountDto.from(account));
         } catch (IllegalStateException e) {
             log.error(e.getMessage());
             return ResponseEntity.notFound().build();
         }
-
     }
 
-    @GetMapping("/accounts/renew/{gameName}/{tagLine}")
-    public ResponseEntity<?> renewAccount(@PathVariable("gameName") String gameName
+    @PatchMapping("/accounts/renew/{gameName}/{tagLine}")
+    public ResponseEntity<RiotAccountDto> renewAccount(@PathVariable("gameName") String gameName
             , @PathVariable("tagLine") String tagLine) {
-
         try {
             RiotAccount account = riotAccountManager.renewAccount(gameName, tagLine);
-            RiotAccountResponse response = new RiotAccountResponse(account.getPuuid(),
-                    account.getGameName(),
-                    account.getTagLine(),
-                    String.valueOf(account.getId()));
-
-            // 응답에는 포함되지 않더라도 갱신 요청 DB에 반영 이후 새로고침으로 해결 예정
-            tftLeagueEntryManager.renewEntry(account.getPuuid());
-
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(RiotAccountDto.from(account));
         } catch (IllegalStateException e) {
+            log.error("Failed to renew account for {}#{}: {}", gameName, tagLine, e.getMessage());
             return ResponseEntity.notFound().build();
         }
-
     }
-
 }
