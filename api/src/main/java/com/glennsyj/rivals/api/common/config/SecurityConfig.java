f13f2185b82af2bc9ad6ae5c0cdbc07cbcf77d60
glenn-syj <glenn.syj@gmail.com>
2025-06-21 00:49:25 +0900

    Allow PATCH method on SecurityConfig CORS

--- api/src/main/java/com/glennsyj/rivals/api/common/config/SecurityConfig.java
+++ api/src/main/java/com/glennsyj/rivals/api/common/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // CORS 허용할 경로
                         .allowedOrigins("http://localhost:3000", "http://frontend:3000", "https://rivals.r-e.kr") // 허용할 출처(origin)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
