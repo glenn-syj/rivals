17a05b465867377b087327851145237301ee818a
glenn-syj <glenn.syj@gmail.com>
2025-06-22 23:56:26 +0900

    Refactor TftMatchController and integrate TftFacade
    - Modify related test cases to use TftFacade

--- api/src/main/java/com/glennsyj/rivals/api/tft/controller/TftMatchController.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/controller/TftMatchController.java
@@ -1,43 +1,31 @@
 package com.glennsyj.rivals.api.tft.controller;
 
-import com.glennsyj.rivals.api.riot.entity.RiotAccount;
-import com.glennsyj.rivals.api.riot.service.RiotAccountManager;
-import com.glennsyj.rivals.api.tft.entity.match.TftMatch;
+import com.glennsyj.rivals.api.tft.facade.TftFacade;
 import com.glennsyj.rivals.api.tft.model.match.TftRecentMatchDto;
-import com.glennsyj.rivals.api.tft.service.TftMatchManager;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path="/api/v1/tft/matches")
+@RequestMapping("/api/v1/tft/matches")
 public class TftMatchController {
+    private final TftFacade tftFacade;
 
-    private TftMatchManager tftMatchManager;
-    private RiotAccountManager riotAccountManager;
-
-    public TftMatchController(TftMatchManager tftMatchManager, RiotAccountManager riotAccountManager) {
-        this.tftMatchManager = tftMatchManager;
-        this.riotAccountManager = riotAccountManager;
+    public TftMatchController(TftFacade tftFacade) {
+        this.tftFacade = tftFacade;
     }
 
-    @GetMapping(path="/{gameName}/{tagLine}")
-    public ResponseEntity<List<TftRecentMatchDto>> findOrCreateRecentTftMatches(@PathVariable String gameName, @PathVariable String tagLine) {
+    @GetMapping("/{gameName}/{tagLine}")
+    public ResponseEntity<List<TftRecentMatchDto>> getRecentMatches(
+            @PathVariable String gameName,
+            @PathVariable String tagLine) {
         try {
-            RiotAccount account = riotAccountManager.findOrRegisterAccount(gameName, tagLine);
-            List<TftMatch> matches = tftMatchManager.findOrCreateRecentTftMatches(account.getId(), account.getPuuid());
-
-            List<TftRecentMatchDto> matchDtos = matches.stream()
-                    .map((match) -> TftRecentMatchDto.from(account.getPuuid(), match)).toList();
-
-            return ResponseEntity.ok(matchDtos);
+            List<TftRecentMatchDto> dtos = tftFacade.findAndProcessMatches(gameName, tagLine);
+            return ResponseEntity.ok(dtos);
         } catch (IllegalStateException e) {
-            return ResponseEntity.badRequest().body(null);
+            return ResponseEntity.badRequest().build();
         }
     }
-
 }
