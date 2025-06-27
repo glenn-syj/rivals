199ab37eeddcfd27b92e837e722e1cf7927ce48c
glenn-syj <glenn.syj@gmail.com>
2025-06-27 16:24:58 +0900

    Add Spring Boot Actuator  monitoring for dev environment
    - Include `spring-boot-starter-actuator` dependency for monitoring and management features
    - Update `application-dev.yml` to expose all management endpoints and show health details in development environment

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.7.3")
 	testImplementation("org.testcontainers:testcontainers:1.19.7")
