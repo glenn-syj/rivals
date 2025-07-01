a4bd18cb81da9f4e2c941cfb6378cd17cc984512
glenn-syj <glenn.syj@gmail.com>
2025-07-01 22:33:30 +0900

    Add methods to TftBadgeProgressRepository for batch retrieval of badge progress
    - Introduce a new query method to fetch badge progress by multiple Riot accounts
    - Implement a default method to return badge progress as a Map grouped by account ID
    - Update tests to validate the new functionality for multiple accounts
    
    Ref: #33

--- api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftBadgeProgressRepository.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/repository/TftBadgeProgressRepository.java
@@ -3,10 +3,14 @@ package com.glennsyj.rivals.api.tft.repository;
 import com.glennsyj.rivals.api.riot.entity.RiotAccount;
 import com.glennsyj.rivals.api.tft.entity.achievement.TftBadgeProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public interface TftBadgeProgressRepository extends JpaRepository<TftBadgeProgress, Long> {
@@ -19,4 +23,13 @@ public interface TftBadgeProgressRepository extends JpaRepository<TftBadgeProgre
     List<TftBadgeProgress> findByRiotAccountAndIsActiveTrue(RiotAccount riotAccount);
 
     List<TftBadgeProgress> findByRiotAccount(RiotAccount riotAccount);
+
+    @Query("SELECT bp FROM TftBadgeProgress bp WHERE bp.riotAccount IN :accounts")
+    List<TftBadgeProgress> findByRiotAccountIn(List<RiotAccount> accounts);
+
+    // Return Type을 Map으로 이용하기 위해서 작성
+    default Map<Long, List<TftBadgeProgress>> findMapByRiotAccountIn(List<RiotAccount> accounts) {
+        return findByRiotAccountIn(accounts).stream()
+                .collect(Collectors.groupingBy(bp -> bp.getRiotAccount().getId()));
+    }
 }
