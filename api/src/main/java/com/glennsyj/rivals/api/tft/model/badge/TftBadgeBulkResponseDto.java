385d0fc413ac2791b5398e883318b8be05773101
glenn-syj <glenn.syj@gmail.com>
2025-07-01 23:20:10 +0900

    Change TftBadgeBulkResponseDto to use Map

--- api/src/main/java/com/glennsyj/rivals/api/tft/model/badge/TftBadgeBulkResponseDto.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/model/badge/TftBadgeBulkResponseDto.java
@@ -1,8 +1,9 @@
 package com.glennsyj.rivals.api.tft.model.badge;
 
 import java.util.List;
+import java.util.Map;
 
 public record TftBadgeBulkResponseDto(
-    List<TftParticipantBadgeDto> participantsWithBadge
+        Map<String, List<TftBadgeDto>> badgesOnPuuid
 ) {
 }
