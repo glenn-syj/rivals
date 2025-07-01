056355bd797977dd578ab5c2cd5eddfdb5df9aaf
glenn-syj <glenn.syj@gmail.com>
2025-07-02 00:25:06 +0900

    Add validation for  TftBadgeBulkRequestDto
    - Add `@NotEmpty` annotation to validate PUUIDs field is not empty
    
    Ref: #33

--- api/src/main/java/com/glennsyj/rivals/api/tft/model/badge/TftBadgeBulkRequestDto.java
+++ api/src/main/java/com/glennsyj/rivals/api/tft/model/badge/TftBadgeBulkRequestDto.java
@@ -1,8 +1,10 @@
 package com.glennsyj.rivals.api.tft.model.badge;
 
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record TftBadgeBulkRequestDto(
+    @NotEmpty(message = "PUUIDs cannot be empty")
     List<String> puuids
 ) {
 }
